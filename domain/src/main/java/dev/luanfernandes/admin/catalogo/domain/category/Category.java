a8fa14bff787334885dbd20b2cdc78de6387cfb0
Luan Fernandes <souluanf@icloud.com>
2024-01-05 20:15:48 -0300

    tornando Category em um AggregateRoot

--- domain/src/main/java/dev/luanfernandes/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/dev/luanfernandes/admin/catalogo/domain/category/Category.java
@@ -1,10 +1,10 @@
 package dev.luanfernandes.admin.catalogo.domain.category;
 
-import java.time.Instant;
-import java.util.UUID;
+import dev.luanfernandes.admin.catalogo.domain.AggregateRoot;
 
-public class Category {
-    private String id;
+import java.time.Instant;
+
+public class Category extends AggregateRoot<CategoryID> {
     private String name;
     private String description;
     private boolean active;
@@ -13,20 +13,20 @@ public class Category {
     private Instant deletedAt;
 
     private Category(
-            final String id,
-            final String name,
-            final String description,
-            final boolean active,
-            final Instant createdAt,
-            final Instant updatedAt,
-            final Instant deletedAt) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreationDate,
+            final Instant anUpdateDate,
+            final Instant aDeleteDate) {
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreationDate;
+        this.updatedAt = anUpdateDate;
+        this.deletedAt = aDeleteDate;
     }
 
     public static Category newCategory(
@@ -34,12 +34,12 @@ public class Category {
             final String description,
             final boolean active) {
 
-        final var id = UUID.randomUUID().toString();
+        final var id = CategoryID.unique();
         final var now = Instant.now();
         return new Category(id, name, description, active, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
