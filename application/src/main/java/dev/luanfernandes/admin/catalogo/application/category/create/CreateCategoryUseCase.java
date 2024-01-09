b3ea34a783db368cff43256a11a02fbff9d4c6db
Luan Fernandes <souluanf@icloud.com>
2024-01-08 21:02:17 -0300

    Incluindo a biblioteca vavr

--- application/src/main/java/dev/luanfernandes/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/dev/luanfernandes/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,5 +1,8 @@
 package dev.luanfernandes.admin.catalogo.application.category.create;
 
 import dev.luanfernandes.admin.catalogo.application.UseCase;
+import dev.luanfernandes.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {}
+public abstract class CreateCategoryUseCase
+        extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {}
--- application/src/main/java/dev/luanfernandes/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/dev/luanfernandes/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
@@ -3,6 +3,7 @@ package dev.luanfernandes.admin.catalogo.application.category.create;
 import dev.luanfernandes.admin.catalogo.domain.category.Category;
 import dev.luanfernandes.admin.catalogo.domain.category.CategoryGateway;
 import dev.luanfernandes.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 import java.util.Objects;
 
 public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
@@ -13,7 +14,7 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
     }
 
     @Override
-    public CreateCategoryOutput execute(final CreateCategoryCommand aCommand) {
+    public Either<Notification, CreateCategoryOutput> execute(final CreateCategoryCommand aCommand) {
         final var aName = aCommand.name();
         final var aDescription = aCommand.description();
         final var isActive = aCommand.active();
