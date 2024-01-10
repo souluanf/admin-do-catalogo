81c503a1cbcd411e23d6a49817f687682ca24e6f
Luan Fernandes <souluanf@icloud.com>
2024-01-09 22:21:18 -0300

    Configurando os profiles do springboot

--- infrastructure/src/main/java/dev/luanfernandes/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/dev/luanfernandes/admin/catalogo/infrastructure/Main.java
@@ -1,5 +1,9 @@
 package dev.luanfernandes.admin.catalogo.infrastructure;
 
+import static java.lang.System.setProperty;
+import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
+
 import dev.luanfernandes.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        setProperty(DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
