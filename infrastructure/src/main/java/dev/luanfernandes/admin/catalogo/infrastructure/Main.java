0fe996c8a207b535b905dee9073515ae78d20ae2
Luan Fernandes <souluanf@icloud.com>
2024-01-09 23:55:13 -0300

    Configurando o datasource de testes da aplicação

--- infrastructure/src/main/java/dev/luanfernandes/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/dev/luanfernandes/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,6 @@
 package dev.luanfernandes.admin.catalogo.infrastructure;
 
 import static java.lang.System.setProperty;
-import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
 import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
 
 import dev.luanfernandes.admin.catalogo.infrastructure.configuration.WebServerConfig;
@@ -11,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "development");
         setProperty(DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
--- infrastructure/src/test/java/dev/luanfernandes/admin/catalogo/infrastructure/MainTest.java
+++ infrastructure/src/test/java/dev/luanfernandes/admin/catalogo/infrastructure/MainTest.java
@@ -3,11 +3,13 @@ package dev.luanfernandes.admin.catalogo.infrastructure;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 class MainTest {
 
     @Test
     void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Main main = new Main();
         Main.main(new String[] {});
         assertNotNull(main);
