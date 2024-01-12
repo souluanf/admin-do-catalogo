23504f9a3503cc1581c08f76a5fc406a98ea61bd
Luan Fernandes <souluanf@icloud.com>
2024-01-11 22:56:24 -0300

    Criando a entidade jpa da categoria

--- domain/src/main/java/dev/luanfernandes/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/dev/luanfernandes/admin/catalogo/domain/category/Category.java
@@ -36,8 +36,19 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         return new Category(id, name, description, active, now, now, deletedAt);
     }
 
+    public static Category with(
+            final CategoryID anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt) {
+        return new Category(anId, name, description, active, createdAt, updatedAt, deletedAt);
+    }
+
     public static Category with(final Category aCategory) {
-        return new Category(
+        return with(
                 aCategory.getId(),
                 aCategory.getName(),
                 aCategory.getDescription(),
